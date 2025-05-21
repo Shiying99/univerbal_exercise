51c06060d302522bbe32dd80be9f004b49ef39e0
Shiying Li <lisha992610@gmail.com>
2025-05-21 23:19:54 +0200

    Optimized the search bar and dropdown suggestion

--- app/features/search/index.tsx
+++ app/features/search/index.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useRef } from 'react';
+import React, { ReactNode, useRef, useState } from 'react';
 import {
   StyleProp,
   StyleSheet,
@@ -6,6 +6,7 @@ import {
   View,
   ViewStyle,
   Text,
+  TouchableOpacity,
 } from 'react-native';
 import { inputValue$, suggestions$ } from './state';
 import { useAtom, useAtomValue } from 'jotai';
@@ -20,27 +21,48 @@ export function Search({ style }: SearchProps): ReactNode {
   const [inputValue, setInputValue] = useAtom(inputValue$);
   const suggestions = useAtomValue(loadable(suggestions$));
 
+  const [isDropDownVisible, setDropdownVisible] = useState(true);
+
+  const onOptionPress = (option: string) => {
+    setInputValue(option);
+    setDropdownVisible(false);
+  };
+
+  const handleSearch = () => {};
+
   return (
     <View style={[searchStyles.root, style]}>
       <TextInput
         ref={inputRef}
-        style={{ height: 40, borderColor: 'red', borderWidth: 2 }}
+        style={searchStyles.input}
         placeholder="type to search..."
         onChangeText={setInputValue}
         value={inputValue}
       />
 
-      {!inputValue ? null : (
-        <View style={searchStyles.suggestions}>
-          {suggestions.state !== 'hasData'
-            ? null
-            : suggestions.data.map((it) => (
-                <View style={searchStyles.suggestionEntry}>
-                  <Text>{it.title}</Text>
-                </View>
-              ))}
+      {inputValue && suggestions.state === 'hasData' && suggestions.data && (
+        <View
+          style={[
+            searchStyles.suggestions,
+            { display: isDropDownVisible ? 'flex' : 'none' },
+          ]}
+        >
+          {suggestions.data.map((it, index) => (
+            <View key={index} style={searchStyles.suggestionEntry}>
+              <TouchableOpacity onPress={() => onOptionPress(it.title)}>
+                <Text>{it.title}</Text>
+              </TouchableOpacity>
+            </View>
+          ))}
         </View>
       )}
+
+      <TouchableOpacity
+        style={searchStyles.searchButton}
+        onPress={handleSearch}
+      >
+        <Text style={searchStyles.buttonText}>Search</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -48,17 +70,40 @@ export function Search({ style }: SearchProps): ReactNode {
 const searchStyles = StyleSheet.create({
   root: {},
 
-  input: {},
+  input: {
+    height: 40,
+    borderColor: 'black',
+    backgroundColor: 'lavender',
+    borderWidth: 2,
+    paddingLeft: 12,
+    borderRadius: 10,
+  },
 
   suggestions: {
     width: '100%',
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: 'relative',
     backgroundColor: 'white',
     borderWidth: 2,
-    borderColor: 'yellow',
+    borderColor: 'black',
+    paddingLeft: 12,
   },
 
   suggestionEntry: {},
+
+  searchButton: {
+    backgroundColor: '#007BFF',
+    height: 40,
+    paddingVertical: 10,
+    paddingHorizontal: 16,
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
